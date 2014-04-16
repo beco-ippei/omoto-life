8b3aab9e2177b22a4c8f23678e7b271f976eb15e
beco-ippei <beco.ippei@gmail.com>
2014-04-16 22:57:11 +0900

    adjust items page layout, add image rotate button

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var current_rotation = 0;
+var rotateImage = function(deg) {
+  var img_frame = $('.image-frame');
+  var rotation = current_rotation + deg;
+  if (Math.abs(rotation) == 360) {
+    rotation = 0;
+  }
+  img_frame.css('-webkit-transform', 'rotate('+rotation+'deg)');
+  current_rotation = rotation;
+}
+$('.rotate-r').on('click', function() {
+  rotateImage(90);
+});
+$('.rotate-l').on('click', function() {
+  rotateImage(-90);
+});
+
