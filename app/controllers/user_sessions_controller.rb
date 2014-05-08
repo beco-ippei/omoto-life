c268eafef30002f795067a66906e7550636b1fd4
beco-ippei <beco.ippei@gmail.com>
2014-05-08 23:42:58 +0900

    redirect back to prev page after login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,21 +3,23 @@ class UserSessionsController < ApplicationController
 
   def new
     @user = User.new
+    session[:return_to_url] = session[:previous_url]
   end
 
   def create
     if @user = login(params[:email], params[:password], params[:remember])
-      redirect_back_or_to :users, notice: 'Login successfull.'
+      redirect_back_or_to :root, notice: 'ログインしました'
+      session[:return_to_url] = nil
     else
       @user = User.new(email: params[:email])
-      flash.now[:alert] = "Login failed."
+      flash.now[:alert] = "ログインできません。メールアドレスかパスワードが間違っています"
       render action: :new
     end
   end
 
   def destroy
     logout
-    redirect_to :users, notice: 'Logged out!'
+    redirect_to :root, notice: 'ログアウトしました'
   end
 
   private
