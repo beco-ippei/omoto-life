c396a1d7b3973f1a7e7877933e9ff566dc3268bb
beco-ippei <beco.ippei@gmail.com>
2014-07-07 23:41:29 +0900

    adjust ui/links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def root_now?
+    /^\/$/.match request.original_fullpath
+  end
+
+  def items_now?
+    /^\/items$/.match request.original_fullpath
+  end
+
+  def users_now?
+    /^\/users$/.match request.original_fullpath
+  end
+
+  def upload_now?
+    /^\/items\/new$/.match request.original_fullpath
+  end
 end
